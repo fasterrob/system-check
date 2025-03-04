f6ea08512bf0d0fb4e6df8c332536f56407e111c
Naiyana Norkaew <naiyana.nk14@gmail.com>
2025-03-04 17:24:06 +0700

    edit ips and threat front

--- src/views/Ips.vue
+++ src/views/Ips.vue
@@ -1,172 +1,108 @@
 <template>
-    <v-container>
-      <v-row justify="end" class="mb-3">
-        <!-- Search Bar -->
-        <v-col cols="12" md="3" class="d-flex align-end">
-          <v-text-field
-            v-model="search"
-            label="Search"
-            variant="outlined"
-            append-inner-icon="mdi-magnify"
-            clearable
-            dense
-          ></v-text-field>
-        </v-col>
+  <v-container>
+    <v-row justify="end" class="mb-3">
+      <!-- Search Bar -->
+      <v-col cols="12" md="3" class="d-flex align-end">
+        <v-text-field
+          v-model="search_input"
+          @keyup.enter="fetchData"
+          label="Search"
+          variant="outlined"
+          append-inner-icon="mdi-magnify"
+          clearable
+          dense
+        ></v-text-field>
+      </v-col>
 
-        <!-- Date Range Picker -->
-        <v-col cols="3">
-          <v-menu v-model="startDatePicker" :close-on-content-click="false">
-            <template v-slot:activator="{ props }">
-              <v-text-field
-                v-model="formattedStartDate"
-                label="Start Date"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-bind="props"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="startDate"
-              @update:modelValue="updateFormattedDate"
-            ></v-date-picker>
-          </v-menu>
-        </v-col>
-        <v-col cols="3">
-          <v-menu v-model="endDatePicker" :close-on-content-click="false">
-            <template v-slot:activator="{ props }">
-              <v-text-field
-                v-model="formattedEndDate"
-                label="End Date"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-bind="props"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="endDate"
-              @update:modelValue="updateFormattedDate"
-            ></v-date-picker>
-          </v-menu>
-        </v-col>
-  
-      </v-row>
-  
-      <!-- Tabs -->
-      <v-card>
-        <v-window v-model="tab">
-          <v-window-item value="total">
-            <v-card class="pa-4">
-              <v-card-title>IPS Log</v-card-title>
-              <v-data-table
-                :headers="deniedHeader"
-                :items="deniedDataTable"
-                :items-per-page="5"
-                class="elevation-1"
-              ></v-data-table>
-            </v-card>
-          </v-window-item>
-        </v-window>
-      </v-card>
-    </v-container>
-  </template>
-  
-  <script>
-  import api from '@/plugins/axios';
-  import Chart from 'chart.js/auto';
-  import downsamplePlugin from 'chartjs-plugin-downsample';
-  
-  // Ensure the plugin is correctly registered
-  if (downsamplePlugin && downsamplePlugin.id) {
-    Chart.register(downsamplePlugin);
-  } else {
-    console.warn('Downsample plugin not properly imported');
-  }
-  
-  export default {
-    data() {
-      return {
-        tab: 'total',
-        startDate: new Date(new Date().setMonth(new Date().getMonth() - 1)),
-        endDate: new Date(),
-        formattedStartDate: '01-DEC-24',
-        formattedEndDate: '31-DEC-24',
-        startDatePicker: false,
-        endDatePicker: false,
-        deniedData: [],
-        deniedDataTable: [],
-        deniedHeader: [
-          { title: 'Severity', key: 'L_DATE' },
-          { title: 'Attack ID', key: 'REMIP' },
-          { title: 'Incidents', key: 'LOGDESC' },
-        ],
-      };
-    },
-    methods: {
-      formatDate(date) {
-        if (!(date instanceof Date)) return '';
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = date
-          .toLocaleString('en-US', { month: 'short' })
-          .toUpperCase();
-        const year = date.getFullYear().toString().slice(-2);
-        return `${day}-${month}-${year}`;
-      },
-      async fetchData(type) {
-        console.log(
-          `Fetching ${type} data from ${this.formattedStartDate} to ${this.formattedEndDate}...`,
-        );
-        try {
-          let response = await api.get(`/firewall/${type}-`, {
-            params: {
-              start_date: this.formattedStartDate,
-              end_date: this.formattedEndDate,
-            },
-          });
-  
-          let rawData = response.data.site_name;
-          
-        } catch (error) {
-          console.error(`Error fetching ${type} data:`, error);
-        }
-      },
-      aggregateData(data, dateKey, valueKey) {
-        let aggregated = {};
-        data.forEach((row) => {
-          let timestamp = row[dateKey].slice(0, 13);
-          if (!aggregated[timestamp]) {
-            aggregated[timestamp] = { L_DATE: timestamp, [valueKey]: 0 };
-          }
-          aggregated[timestamp][valueKey] += parseFloat(row[valueKey]);
-        });
-        return Object.values(aggregated);
-      },
-      renderChart(chartRef, label, data, dataKey) {
-        if (this[chartRef]) this[chartRef].destroy();
-  
-        let ctx = this.$refs[chartRef].getContext('2d');
-        this[chartRef] = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: data.map((d) => d.L_DATE),
-            datasets: [
-              {
-                label,
-                data: data.map((d) => d[dataKey]),
-                borderColor: 'blue',
-                borderWidth: 1,
-              },
-            ],
-          },
-          options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-              downsample: { enabled: true, threshold: 5000, auto: true },
-            },
+      <!-- Date Range Picker -->
+      <v-col cols="3">
+        <v-text-field
+          v-model="startDate"
+          label="Start Date"
+          type="date"
+          outlined
+        ></v-text-field>
+      </v-col>
+      <v-col cols="3">
+        <v-text-field
+          v-model="endDate"
+          label="End Date"
+          type="date"
+          @keyup.enter="fetchData"
+          outlined
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <!-- Antivirus Table -->
+    <v-card elevation="2">
+      <v-card-title>IPS Log</v-card-title>
+      <v-data-table
+        :headers="ipsHeader"
+        :items="ipsDataTable"
+        :items-per-page="10"
+        class="elevation-1"
+        density="compact"
+      >
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const search_input = ref('');
+    const startDate = ref(
+      new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString().split('T')[0]
+    );
+    const endDate = ref(new Date().toISOString().split('T')[0]);
+    const ipsDataTable = ref([]);
+    const ipsHeader = ref([
+      { title: 'Date', key: 'Date' },
+      { title: 'User', key: 'User' },
+      { title: 'Source', key: 'Source' },
+      { title: 'Destination', key: 'Destination' },
+      { title: 'Severity', key: 'Severity' },
+      { title: 'Attack ID', key: 'AttackID' },
+      { title: 'Incidents', key: 'Counts' },
+    ]);
+
+    const fetchData = async () => {
+      try {
+        let response = await api.get(`/firewall/ips-log`, {
+          params: {
+            start_date: startDate.value,
+            end_date: endDate.value,
+            search_input: search_input.value,
           },
         });
-      },
-    },
+        ipsDataTable.value = response.data.site_name;
+      } catch (error) {
+        console.error(`Error fetching ips log data:`, error);
+      }
+    };
+
+    onMounted(fetchData);
+
+    return {
+      search_input,
+      startDate,
+      endDate,
+      ipsDataTable,
+      ipsHeader,
+      fetchData,
+    };
+  },
 };
-  </script>
-  
+</script>
+
+<style scoped>
+.v-container {
+  max-width: 1200px;
+  margin: auto;
+}
+</style>
--- src/views/Threat.vue
+++ src/views/Threat.vue
@@ -4,7 +4,8 @@
         <!-- Search Bar -->
         <v-col cols="12" md="3" class="d-flex align-end">
           <v-text-field
-            v-model="search"
+            v-model="search_input"
+            @keyup.enter="fetchData"
             label="Search"
             variant="outlined"
             append-inner-icon="mdi-magnify"
@@ -12,161 +13,93 @@
             dense
           ></v-text-field>
         </v-col>
-
+  
         <!-- Date Range Picker -->
         <v-col cols="3">
-          <v-menu v-model="startDatePicker" :close-on-content-click="false">
-            <template v-slot:activator="{ props }">
-              <v-text-field
-                v-model="formattedStartDate"
-                label="Start Date"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-bind="props"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="startDate"
-              @update:modelValue="updateFormattedDate"
-            ></v-date-picker>
-          </v-menu>
+          <v-text-field
+            v-model="startDate"
+            label="Start Date"
+            type="date"
+            outlined
+          ></v-text-field>
         </v-col>
         <v-col cols="3">
-          <v-menu v-model="endDatePicker" :close-on-content-click="false">
-            <template v-slot:activator="{ props }">
-              <v-text-field
-                v-model="formattedEndDate"
-                label="End Date"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-bind="props"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="endDate"
-              @update:modelValue="updateFormattedDate"
-            ></v-date-picker>
-          </v-menu>
+          <v-text-field
+            v-model="endDate"
+            label="End Date"
+            type="date"
+            @keyup.enter="fetchData"
+            outlined
+          ></v-text-field>
         </v-col>
-  
       </v-row>
   
-      <!-- Tabs -->
-      <v-card>
-        <v-window v-model="tab">
-          <v-window-item value="total">
-            <v-card class="pa-4">
-              <v-card-title>Threat</v-card-title>
-              <v-data-table
-                :headers="deniedHeader"
-                :items="deniedDataTable"
-                :items-per-page="5"
-                class="elevation-1"
-              ></v-data-table>
-            </v-card>
-          </v-window-item>
-        </v-window>
+      <!-- Antivirus Table -->
+      <v-card elevation="2">
+        <v-card-title>Threat</v-card-title>
+        <v-data-table
+          :headers="threatHeader"
+          :items="threatDataTable"
+          :items-per-page="10"
+          class="elevation-1"
+          density="compact"
+        >
+        </v-data-table>
       </v-card>
     </v-container>
   </template>
   
   <script>
   import api from '@/plugins/axios';
-  import Chart from 'chart.js/auto';
-  import downsamplePlugin from 'chartjs-plugin-downsample';
-  
-  // Ensure the plugin is correctly registered
-  if (downsamplePlugin && downsamplePlugin.id) {
-    Chart.register(downsamplePlugin);
-  } else {
-    console.warn('Downsample plugin not properly imported');
-  }
+  import { ref, onMounted } from 'vue';
   
   export default {
-    data() {
-      return {
-        tab: 'total',
-        startDate: new Date(new Date().setMonth(new Date().getMonth() - 1)),
-        endDate: new Date(),
-        formattedStartDate: '01-DEC-24',
-        formattedEndDate: '31-DEC-24',
-        startDatePicker: false,
-        endDatePicker: false,
-        deniedData: [],
-        deniedDataTable: [],
-        deniedHeader: [
-          { title: 'Threat Level', key: 'L_DATE' },
-          { title: 'Report Type', key: 'REMIP' },
-          { title: 'Counts', key: 'LOGDESC' },
-        ],
-      };
-    },
-    methods: {
-      formatDate(date) {
-        if (!(date instanceof Date)) return '';
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = date
-          .toLocaleString('en-US', { month: 'short' })
-          .toUpperCase();
-        const year = date.getFullYear().toString().slice(-2);
-        return `${day}-${month}-${year}`;
-      },
-      async fetchData(type) {
-        console.log(
-          `Fetching ${type} data from ${this.formattedStartDate} to ${this.formattedEndDate}...`,
-        );
+    setup() {
+      const search_input = ref('');
+      const startDate = ref(
+        new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString().split('T')[0]
+      );
+      const endDate = ref(new Date().toISOString().split('T')[0]);
+      const threatDataTable = ref([]);
+      const threatHeader = ref([
+        { title: 'Severity', key: 'L_DATE' },
+        { title: 'Attack ID', key: 'REMIP' },
+        { title: 'Incidents', key: 'FAIL_COUNTS' },
+      ]);
+  
+      const fetchData = async () => {
         try {
-          let response = await api.get(`/firewall/${type}-`, {
+          let response = await api.get(`/firewall/denied-access`, {
             params: {
-              start_date: this.formattedStartDate,
-              end_date: this.formattedEndDate,
+              start_date: startDate.value,
+              end_date: endDate.value,
+              search_input: search_input.value,
             },
           });
-  
-          let rawData = response.data.site_name;
-          
+          threatDataTable.value = response.data.site_name;
         } catch (error) {
-          console.error(`Error fetching ${type} data:`, error);
+          console.error(`Error fetching denied access data:`, error);
         }
-      },
-      aggregateData(data, dateKey, valueKey) {
-        let aggregated = {};
-        data.forEach((row) => {
-          let timestamp = row[dateKey].slice(0, 13);
-          if (!aggregated[timestamp]) {
-            aggregated[timestamp] = { L_DATE: timestamp, [valueKey]: 0 };
-          }
-          aggregated[timestamp][valueKey] += parseFloat(row[valueKey]);
-        });
-        return Object.values(aggregated);
-      },
-      renderChart(chartRef, label, data, dataKey) {
-        if (this[chartRef]) this[chartRef].destroy();
+      };
   
-        let ctx = this.$refs[chartRef].getContext('2d');
-        this[chartRef] = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: data.map((d) => d.L_DATE),
-            datasets: [
-              {
-                label,
-                data: data.map((d) => d[dataKey]),
-                borderColor: 'blue',
-                borderWidth: 1,
-              },
-            ],
-          },
-          options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-              downsample: { enabled: true, threshold: 5000, auto: true },
-            },
-          },
-        });
-      },
+      onMounted(fetchData);
+  
+      return {
+        search_input,
+        startDate,
+        endDate,
+        threatDataTable,
+        threatHeader,
+        fetchData,
+      };
     },
-};
+  };
   </script>
+  
+  <style scoped>
+  .v-container {
+    max-width: 1200px;
+    margin: auto;
+  }
+  </style>
   
